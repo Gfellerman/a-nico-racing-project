f53c5972283e354cb7326941a8ba3f974602bd86
Gfellerman <ngfeller@gmail.com>
2025-10-15 19:11:20 +0100

    fix: Add RouterProvider setup for navigation

--- frontend-new/src/main.tsx
+++ frontend-new/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import ProjectPage from './pages/ProjectPage.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/projects/:slug', element: <ProjectPage /> },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
