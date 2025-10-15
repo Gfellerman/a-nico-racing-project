b1c69272d8d526b12810428994beed9ccb5ff506
Gfellerman <ngfeller@gmail.com>
2025-10-15 17:15:20 +0100

    fix: Remove conflicting CORS config from server.js - keep only in middlewares.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -5,14 +5,4 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS', ['key1', 'key2']),
   },
-  cors: {
-    enabled: true,
-    headers: '*',
-    origin: [
-      'http://localhost:5173',
-      'https://organic-space-garbanzo-r4xwr7q449gv35474-5173.app.github.dev'
-    ],
-    methods: ['GET','POST','PUT','PATCH','DELETE','HEAD','OPTIONS'],
-    keepHeaderOnError: true,
-  },
 });
