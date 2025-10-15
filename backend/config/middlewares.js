845c70a10cbba7916b57e81fa9b17d5218b396be
Gfellerman <ngfeller@gmail.com>
2025-10-15 17:14:46 +0100

    fix: Update CORS configuration to resolve frontend-backend communication issues

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -5,10 +5,16 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://localhost:3000'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:3000', 
+        'https://organic-space-garbanzo-r4xwr7q449gv35474-5173.app.github.dev',
+        'https://gfellerman.github.io'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
       keepHeaderOnError: true,
+      credentials: true,
     },
   },
   'strapi::poweredBy',
